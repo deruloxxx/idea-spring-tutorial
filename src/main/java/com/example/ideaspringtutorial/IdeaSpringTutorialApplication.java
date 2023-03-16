87b3e0c18da785a112e1b146335b4e6beebdcf01
ryuyauetake <ryuya@anymindgroup.com>
2023-03-16 23:47:56 +0900

    spring initializr file

---
+++ src/main/java/com/example/ideaspringtutorial/IdeaSpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.ideaspringtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeaSpringTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeaSpringTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ideaspringtutorial/IdeaSpringTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ideaspringtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeaSpringTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
