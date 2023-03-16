f637937320b1a275ecd29823a6dbcc439203aee4
ryuyauetake <ryuya@anymindgroup.com>
2023-03-17 00:51:20 +0900

    feat: run Spring application

--- src/main/java/com/example/ideaspringtutorial/IdeaSpringTutorialApplication.java
+++ src/main/java/com/example/ideaspringtutorial/IdeaSpringTutorialApplication.java
@@ -3,6 +3,7 @@ package com.example.ideaspringtutorial;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -14,7 +15,7 @@ public class IdeaSpringTutorialApplication {
 	}
 
 	@GetMapping("/hello")
-	public String hello() {
-        return "Hello World!";
-    }
+	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
+    return String.format("Hello %s!", name);
+  }
 }
